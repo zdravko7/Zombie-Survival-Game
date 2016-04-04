dcedd5445db81879e267664e33c3b746b4189c0b
Zdravko Botushanov <zdravko7@abv.bg>
2016-04-04 14:13:37 +0300

    Added UI, fixed zombie increase speed

---
+++ Zombie Survival Game/Assets/SceneChanger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
--- Zombie Survival Game/Assets/Scripts/Zombie Scripts/Zombie.cs
+++ Zombie Survival Game/Assets/Scripts/Zombie Scripts/Zombie.cs
@@ -5,7 +5,7 @@ public class Zombie : MonoBehaviour
 {
 
     public string direction;
-    public float movementSpeed;
+    public float movementSpeed = 5;
     private Vector2 directionV2;
     GameObject Player;
         
@@ -19,20 +19,28 @@ public class Zombie : MonoBehaviour
         {
             case "(0.0, 9.0, 7.0)":
                 direction = "down";
-                directionV2 = Vector2.down; break;
+                directionV2 = Vector2.down; 
+                break;
+
             case "(13.0, 0.0, 7.0)":
                 direction = "left";
-                directionV2 = Vector2.left; break;
+                directionV2 = Vector2.left; 
+                break;
+
             case "(-13.0, 0.0, 7.0)":
                 direction = "right";
-                directionV2 = Vector2.right; break;
+                directionV2 = Vector2.right; 
+                break;
+
             case "(0.0, -8.0, 7.0)":
                 direction = "up";
-                directionV2 = Vector2.up; break;
+                directionV2 = Vector2.up; 
+                break;
         }
         anim.SetTrigger(direction);
 
-        movementSpeed = 5;
+        decimal timePassed = GameObject.Find("TimeKeeper").GetComponent<Timer>().timer;
+        movementSpeed += (float)timePassed / 5;
     }
 
     void Update()
@@ -47,6 +55,7 @@ public class Zombie : MonoBehaviour
         if (col.gameObject.tag == "Player" || col.gameObject.tag == "Projectile")
         {
             Destroy(gameObject);
+            
             if (col.gameObject.tag == "Projectile")
             {
                 GameObject.Find("ScoreKeeper").GetComponent<ScoreKeeper>().score += 1000;
